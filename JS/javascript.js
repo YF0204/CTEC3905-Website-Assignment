cc6195641839afb9f0d7d13c80aeec2171957567
YF0204 <yas_0dst@hotmail.co.uk>
2017-11-22 16:53:59 +0000

    Slideshow, CSS Keyframes & AboutMe page

--- JS/javascript.js
+++ JS/javascript.js
@@ -1,1 +1,31 @@
-javascript file
+
+	var myIndex = 0;
+	carousel();
+
+	function carousel() {
+	var i;
+	var x = document.getElementsByClassName("mySlides");
+	for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";  
+		}
+	myIndex++;
+	if (myIndex > x.length) {myIndex = 1}    
+	x[myIndex-1].style.display = "block";  
+	setTimeout(carousel, 3000);
+		}
+		
+	var myIndex = 0;
+	carousel1();
+
+	function carousel1() {
+	var i;
+	var x = document.getElementsByClassName("Slides");
+	for (i = 0; i < x.length; i++) {
+	x[i].style.display = "none";  
+		}
+	myIndex++;
+	if (myIndex > x.length) {myIndex = 1}    
+	x[myIndex-1].style.display = "block";  
+	setTimeout(carousel1, 3000);
+		}
+				
