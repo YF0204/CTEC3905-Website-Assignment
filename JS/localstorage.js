04140f8291f073f233dddcca78b9d83d2460a3b9
YF0204 <yas_0dst@hotmail.co.uk>
2017-11-30 23:07:07 +0000

    Add Event Listener working! Woo!

---
+++ JS/localstorage.js
@@ -0,0 +1,64 @@
+(function(){
+
+  //let myName = document.getElementById("my-name"); 
+  let getName = document.getElementById("get-name");
+  let userName = document.getElementById("user-name"); 
+  let nameStored = localStorage.name;
+
+  // Displays the name in the console at this stage:
+  console.log(`Name on page load: ${nameStored}`);
+  
+  if(nameStored) {
+    // If there's a name in localStorage, use it:
+    userName.value = `Your Profile: ${nameStored}`;
+    console.log(`Name stored is: ${nameStored}`);
+  }
+  else {
+    // There's no name in localStorage:
+    userName.value = "Enter profile here";
+    console.log(`No name stored`);
+  }
+
+  function PerformGreeting() {
+    if(userName.value === "") {
+      alert("Please enter a name");
+      userName.focus();
+    }
+    // Gets the name the user entered:
+    nameStored = userName.value;
+    
+    // Shows the name in "my-name":
+    userName.value = nameStored;
+    
+    // Puts the name into localStorage:
+    localStorage.name = nameStored;
+    
+    // Displays the name in the console at the final stage:
+    console.log(`New name stored: ${nameStored}`);
+    
+    return false;
+  }
+
+  function clearData() {
+    console.log(`before clear: ${localStorage.name}`);
+
+    // Clears all Local Storage data:
+    localStorage.clear();
+
+    // Clears one key-value pair, in this case 'name':
+    // delete localStorage.name;
+    console.log(`after clear: ${localStorage.name}`);
+  }
+
+  // Listens for a form submit action: 
+  if (typeof event === "undefined") {
+    getName.onsubmit = PerformGreeting; // for Firefox
+  }
+  else {
+    getName.addEventListener("submit", PerformGreeting);
+    event.preventDefault();
+  }
+  // Chrome/IE only
+  getName.addEventListener("reset", clearData);
+
+}());
